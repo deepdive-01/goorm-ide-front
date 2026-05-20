710c3f4b7314e5d1d873a4e9476e80f2bd36455f
최유정 <cleame8922@naver.com>
2026-05-20 21:55:02 +0900

    fix: scrolltotop 삭제 후 다음 pr에서 반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
-import ScrollToTop from '@/components/common/ScrollToTop/ScrollToTop'
 import ExamplePage from '@/pages/ExamplePage'
 
 function App() {
   return (
-    <>
-      <ScrollToTop />
-      <Routes>
-        {/* 기본 라우트 */}
-        <Route path="/*" element={<ExamplePage />} />
+    <Routes>
+      {/* 기본 라우트 */}
+      <Route path="/*" element={<ExamplePage />} />
 
-        {/* Student 안에 있는 파일은 /student의 경로로 시작 - 임시로 <ExamplePage />로 설정, 개발 후 변경하시면 됩니다! */}
-        <Route path="/student/*" element={<ExamplePage />} />
+      {/* Student 안에 있는 파일은 /student의 경로로 시작 - 임시로 <ExamplePage />로 설정, 개발 후 변경하시면 됩니다! */}
+      <Route path="/student/*" element={<ExamplePage />} />
 
-        {/* Teacher 안에 있는 파일은 /teacher의 경로로 시작 - 임시로 <ExamplePage />로 설정, 개발 후 변경하시면 됩니다! */}
-        <Route path="/teacher/*" element={<ExamplePage />} />
-      </Routes>
-    </>
+      {/* Teacher 안에 있는 파일은 /teacher의 경로로 시작 - 임시로 <ExamplePage />로 설정, 개발 후 변경하시면 됩니다! */}
+      <Route path="/teacher/*" element={<ExamplePage />} />
+    </Routes>
   )
 }
 
