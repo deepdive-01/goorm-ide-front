ed65449e13e19b29449c21b056d6989c0d3b5778
최유정 <109840531+cleame8922@users.noreply.github.com>
2026-05-23 00:11:20 +0900

    [COD-23] 머지 후 오류 수정 (#6)
    * fix: cod-23 merge 오류 수정
    
    * fix: 라이브러리 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import ScrollToTop from './components/common/ScrollToTop/ScrollToTop'
 import ExamplePage from '@/pages/ExamplePage'
+import Header from '@/components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <>
       <ScrollToTop />
+      <Header />
       <Routes>
         {/* 기본 라우트 */}
         <Route path="/*" element={<ExamplePage />} />
@@ -16,6 +19,7 @@ function App() {
         {/* Teacher 안에 있는 파일은 /teacher의 경로로 시작 - 임시로 <ExamplePage />로 설정, 개발 후 변경하시면 됩니다! */}
         <Route path="/teacher/*" element={<ExamplePage />} />
       </Routes>
+      <Footer />
     </>
   )
 }
