36b8ad27316f30130b26b50ade21eb5dfdf92ccb
최유정 <cleame8922@naver.com>
2026-06-02 13:58:37 +0900

    fix: 로고 클릭 시 메인으로 이동

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom'
 import { Code2 } from 'lucide-react'
 
 function Logo() {
   return (
-    <header className="flex items-center gap-2">
+    <Link to="/" className="flex items-center gap-2">
       <Code2 className="text-neon-green" />
       <div className="text-light-background text-head3">Code Run</div>
-    </header>
+    </Link>
   )
 }
 
